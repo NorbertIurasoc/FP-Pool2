1f3942a4b2540370a56eafff14ac7265c866c408
Norbert Iurasoc <norbiiurasoc@outlook.com>
2020-12-06 13:55:50 +0100

    produsul nu e bun

---
+++ Pool2-Prob3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pool2_Prob3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int  sum = 0;
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                sum = sum + i;
+                sum++;
+             
+            }
+
+            int pro = 0;
+            for (int i = 0; i < n; i++)
+            {   
+
+                pro = pro * i;
+                pro++;
+
+
+            }
+
+
+
+            Console.WriteLine("Suma numerelor este: " + sum);
+            Console.WriteLine("Produsul numerelor este: " + pro);
+            
+            
+            Console.ReadKey();
+        }
+    }
+}
